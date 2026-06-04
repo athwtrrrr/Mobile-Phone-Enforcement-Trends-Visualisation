4a9f3f0714a278c5f4385871200eb57303f760b7
athwtrrrr <athwtr@gmail.com>
2026-06-04 22:18:22 +1000

    Change load data

--- js/load-data.js
+++ js/load-data.js
@@ -1,5 +1,5 @@
 //load line chart data
-d3.csv("../data/line_chart.csv", d => {
+d3.csv("data/line_chart.csv", d => {
     return{
         year: +d["YEAR"],
         jurisdiction: d["JURISDICTION"],
@@ -28,7 +28,7 @@ d3.csv("../data/line_chart.csv", d => {
 });
 
 //load heatmap
-d3.csv("../data/heatmap.csv", d => {
+d3.csv("data/heatmap.csv", d => {
     return{
         jurisdiction: d["JURISDICTION"],
         age_group: d["AGE_GROUP"],
@@ -53,7 +53,7 @@ d3.csv("../data/heatmap.csv", d => {
     console.log("Error loading heatmap csv file: ", error);
 });
 
-d3.csv("../data/grouped_bar_chart.csv", d => ({
+d3.csv("data/grouped_bar_chart.csv", d => ({
     jurisdiction: d["JURISDICTION"],
     age_group: d["AGE_GROUP"],
     detection: d["DETECTION_METHOD"],
